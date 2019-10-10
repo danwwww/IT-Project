32d9596a8a0b41dcb514782d953c7cc4035f7f65
Shuangyuan Wen <s.wen6@student.unimelb.edu.au>
2019-10-10 21:04:39 +1100

    fix nav bar

--- views/head.js
+++ views/head.js
@@ -54,8 +54,8 @@ document.writeln("                                        <li class=\"menu__item
 document.writeln("                                        <li class=\"dropdown\">");
 document.writeln("                                            <a href=\"#\" class=\"menu__item dropdown-toggle\" data-toggle=\"dropdown\">Upload<b class=\"caret\"></b></a>");
 document.writeln("                                            <ul class=\"dropdown-menu\">");
-document.writeln("                                                <li class=\"menu__item\"><a href=\"upload_artifacts.html\" class=\"menu__link scroll\"><span class=\"menu__helper\">Upload Artifacts</span></a></li>");
-document.writeln("                                                <li class=\"menu__item\"><a href=\"upload_profile.html\" class=\"menu__link scroll\"><span class=\"menu__helper\">Upload Profile</span></a></li>");
+document.writeln("                                                <li class=\"menu__item\"><a href=\"uploadArtifacts\" class=\"menu__link scroll\"><span class=\"menu__helper\">Upload Artifacts</span></a></li>");
+document.writeln("                                                <li class=\"menu__item\"><a href=\"uploadProfiles\" class=\"menu__link scroll\"><span class=\"menu__helper\">Upload Profile</span></a></li>");
 document.writeln("                                            </ul>");
 document.writeln("                                        </li>");
 document.writeln("                                    </ul>");
